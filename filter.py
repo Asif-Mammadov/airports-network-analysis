ca28a85d0528682169146e3ba8cfa34a6167b1cb
RusteenZzZ <67784933+RusteenZzZ@users.noreply.github.com>
2021-12-18 15:45:42 +0400

    Update filter.py
    Clarified the coordinates structure

--- filter.py
+++ filter.py
@@ -78,7 +78,7 @@ european_routes = routes_in_europe(filtered_routes, airports)
 
 print(european_routes)
 
-coords_1 = (52.2296756, 21.0122287)
-coords_2 = (52.406374, 16.9251681)
+coords_1 = (52.2296756, 21.0122287) # latitude , longtitude
+coords_2 = (52.406374, 16.9251681) # latitude , longtitude
 
 print(distance.distance(coords_1, coords_2).km) 
