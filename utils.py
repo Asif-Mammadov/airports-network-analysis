db4bae26b7144d1281dc7764744ec4f0a0d7b573
Asif-Mammadov <asif.mammad@hotmail.com>
2021-12-18 14:55:41 +0400

    added utils for network properties

---
+++ utils.py
@@ -0,0 +1,71 @@
+# TODO : implement shortest path
+import networkx as nx
+
+# Degree Connectivity
+def degree_inflow(G, node):
+    return G.in_degree(node)
+def degree_outflow(G, node):
+    return G.out_degree(node)
+def degree(G, node):
+    return G.degree(node)
+
+
+def _path_length(path):
+    return len(path) - 1
+
+def closeness_centrality(G, node, weight):
+    total_length = 0
+    nodes = list(G.nodes())
+    for dest in nodes:
+        if dest != node:
+            path = nx.shortest_path(G, source=node, target=dest, weight=weight)
+            total_length += _path_length(path)
+    return total_length/len(nodes)
+
+def get_path(G, src, dst, weight):
+    try:
+        return nx.shortest_path(G, src, dst, weight)
+    except:
+        return []
+    
+def get_all_pathes(G, weight):
+    pathes = {}
+    for src in G.nodes():
+        for dst in G.nodes():
+            if src != dst:
+                path = get_path(G, src, dst, weight)
+                if len(path) > 1:
+                    pathes[src + '_' + dst] = path
+    return pathes
+
+def between_centrality(G, node, pathes):
+    count = 0
+    for path in pathes.values():
+        if node in path:
+            count += 1
+    return count
+
+# Network Density
+def network_density(G):
+    nodes = G.nodes()
+    total_degree = 0
+    for node in nodes:
+        total_degree += degree(G, node)
+    return total_degree/(len(nodes) * (len(nodes) - 1))
+
+# Network Diameter
+def network_diameter(G, weight):
+    pathes = get_all_pathes(G, weight)
+    path_len = 0
+    for path in pathes.values():
+        if len(path) > path_len:
+            path_len = len(path)
+    return path_len
+
+# Network Average Path Length
+def network_average_path_length(G, weight):
+    pathes = get_all_pathes(G, weight)
+    total_path_len = 0
+    for path in pathes.values():
+        total_path_len += len(path)
+    return total_path_len/len(pathes)
